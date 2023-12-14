1319aa8a23c975e3bc00e5c8cc78c6bb790b68ca
James Hay <9113187+jameshay218@users.noreply.github.com>
2023-12-14 13:16:08 +0000

    Added stan infrastructure

---
+++ R/seroepirecipes-package.R
@@ -0,0 +1,9 @@
+.datatable.aware <- TRUE
+#' @keywords internal
+#' import Rcpp
+#' import methods
+#' importFrom rstan sampling extract
+#' @useDynLib seroepirecipes, .registration=TRUE
+"_PACKAGE"
+
+NULL
